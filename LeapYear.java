d563dc75339948d45272f93a9ef6cc054d5d794c
Rajendhar9515 <vemula.rajendhar1@gmail.com>
2022-07-15 21:01:28 +0530

    added Day5-problems

---
+++ LeapYear.java
@@ -0,0 +1,20 @@
+package com.bridglab.java.basicproblams;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		int year;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the Year");
+		year = input.nextInt();
+		if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+			System.out.println("Given year is a leap year");
+		} else {
+			System.out.println("Given year is not a leap year");
+		}
+
+	}
+
+}
